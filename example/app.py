e68928b928417b7f9560247f798bcc9e7912fb0f
hustcc <i@hust.cc>
2023-12-19 20:51:41 +0800

    chore: add action, update setup.py

--- example/app.py
+++ example/app.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from streamlit_g2 import g2
 
+st.set_page_config(page_title="streamlit-g2: AntV G2 for Streamlit!")
+
 """
 # Welcome to [streamlit-g2](https://github.com/hustcc/streamlit-g2)!
 
-In the meantime, below are some examples of what you can do with just a few lines of code:
+[G2](https://github.com/antvis/G2) is a visualization grammar for dashboard building, data exploration and storytelling.
+
+This project was created to allow us to render [G2](https://github.com/antvis/G2) charts in streamlit. In the meantime, below are some examples of what you can do with just a few lines of code:
 """
 
 """
--- setup.py
+++ setup.py
@@ -1,19 +1,64 @@
-import setuptools
+import io
+import os
+import sys
+from shutil import rmtree
+from setuptools import Command, find_packages, setup
 
-setuptools.setup(
+here = os.path.abspath(os.path.dirname(__file__))
+
+class UploadCommand(Command):
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        print("✨✨ {0}".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds…")
+            rmtree(os.path.join(here, "dist"))
+            rmtree(os.path.join(here, "build"))
+            rmtree(os.path.join(here, "{0}.egg-info".format("streamlit_g2")))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel distribution…")
+        os.system("{0} setup.py bdist_wheel".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine…")
+        os.system("twine upload dist/*")
+
+        sys.exit()
+
+def read(*names, **kwargs):
+    return io.open(
+        os.path.join(os.path.dirname(__file__), *names),
+        encoding=kwargs.get("encoding", "utf8")
+    ).read()
+
+setup(
     name="streamlit-g2",
-    version="0.0.6",
+    version="0.1.0",
     author="hustcc",
     author_email="i@hust.cc",
     description="Render G2 charts in Streamlit",
-    long_description="Render G2 charts in Streamlit",
+    long_description=read('readme.md'),
     long_description_content_type="text/plain",
+    keywords=["antv", "g2", "streamlit-component", "streamlit-g2"],
     url="https://github.com/hustcc/streamlit-g2",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.6",
     install_requires=[
         "streamlit >= 0.63",
     ],
+    cmdclass={"upload": UploadCommand},
 )
